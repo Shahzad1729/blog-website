0888eec3cea5785b8a3a083f7b1a01eb305dffe1
Shahzad Sayyed <98218245+Shahzad1729@users.noreply.github.com>
2022-07-01 16:08:31 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/blog/contact',include('blog.urls',namespace='blog')),
 
 
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+]
